61b963c3fec7a67ac13fbafadd1b60758c26504a
ALGINJOSE <alginjose7@gmail.com>
2024-03-11 12:29:00 +0530

    jose

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,7 +3,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig = (): TypeOrmModuleOptions => ({
   type: 'postgres',
   host: 'localhost',
-  port: parseInt(process.env.TYPEORM_PORT, 10) || 5432,
+  port: parseInt(process.env.TYPEORM_PORT, 10) || 5433,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
